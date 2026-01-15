c271c9c9a5393598c86f4a238e99e41c455f98a0
Dominique <cometdomi@gmail.com>
2026-01-15 18:44:02 +0100

    fix: Update renderer version to 1.3.1 for timestamped logging feature

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@
 #include "TimestampedLogger.h"
 
 // Version information
-#define RENDERER_VERSION "1.3.0"    // ⭐ v1.3.0: Transfer mode option (VarMax/Fix)
+#define RENDERER_VERSION "1.3.1"    // ⭐ v1.3.1: Timestamped logging
 #define RENDERER_BUILD_DATE __DATE__
 #define RENDERER_BUILD_TIME __TIME__
 // Global renderer instance for signal handler
